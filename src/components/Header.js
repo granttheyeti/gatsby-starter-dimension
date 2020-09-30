2335e755b3ef4e35810e5d93ac4ba10608ca7e60
ulyssesdouglass <ulyssesdouglass7@gmail.com>
2020-09-30 16:47:37 -0700

    Trim copyright, add links to 3 initial projects

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,56 +4,28 @@ import React from 'react'
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-diamond"></span>
+      <span className="icon fa-link"></span>
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
-        <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
-        </p>
+        <p>A portal to the different projects I'm working on</p>
       </div>
     </div>
     <nav>
       <ul>
         <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('intro')
-            }}
-          >
-            Intro
+          <button>
+            <a href="https://www.faithandworkmovement.org/reconciliation">Faith & Work Movement</a>
           </button>
         </li>
         <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('work')
-            }}
-          >
-            Work
+          <button>
+            <a href="http://wonderproject.netlify.app/">Wonder Project</a>
           </button>
         </li>
         <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('about')
-            }}
-          >
-            About
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('contact')
-            }}
-          >
-            Contact
+          <button>
+            <a href="https://anchor.fm/grapenutspodcast">Grape-Nuts Podcast</a>
           </button>
         </li>
       </ul>
